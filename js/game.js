dc73d8aba0efc6480458a987b51779ba0cf651d1
OrionStar25 <chunnushrivastava@gmail.com>
2017-08-19 02:50:25 +0530

    main game made

--- js/game.js
+++ js/game.js
@@ -1,39 +1,62 @@
-const holes = document.querySelectorAll('.hole');
-const moles = document.querySelectorAll('.mole');
-const score = document.querySelector('.score');
+const holes = $('.hole');
+const scoreBoard = $('.score');
+const moles = $('.mole');
 let lastHole;
+var score;
+var duration;
+var points;
 
-function randomTime(min, max)
-{
+function randomTime(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
 
-function randomHole(holes)
-{
-	const idx = Math.floor(Math.random() * holes.length);
-	const hole = holes[idx];
+function randomHoles(holes) {
+	var idx = Math.floor(Math.random() * holes.length);
+	var hole = holes[idx];
+
 	if(hole === lastHole)
 	{
-		return randomHole(holes);
+		randomHoles(holes);
 	}
 	lastHole = hole;
 	return hole;
 }
 
-function peep()
-{
-	const time = randomTime(200,1000);
-	const hole = randomHole(holes);
-	hole.classList.add('up');
-	// setTimeOut(hole.classList.remove('up'),time); //change this
+function peep() {
+	var hole = randomHoles(holes);
+	var time = randomTime(200, 900);
+	hole.classList.add('up');	
+
+	$('.mole').click(function() {
+		score++;
+		points = score;
+		scoreBoard.text(score);
+	});
+
+	setTimeout(function() {
+		hole.classList.remove('up');
+	},time);
 }
 
-function startGame()
-{	
-	till time < 10 sec
-	peep();
+function boink() {
+
 }
 
-$(document).ready(function() {
-	startGame();
-});
+function startGame() {
+	score = 0;
+	duration = 0;
+	scoreBoard.text('0');
+	var lol = setInterval(function(){ 
+		if(duration < 12)
+		{
+			peep();
+			duration += 1;
+		}
+		else
+		{
+			clearInterval(lol);
+			alert("You got " + points + " points!");
+		}
+	}, 1000);
+}
+
